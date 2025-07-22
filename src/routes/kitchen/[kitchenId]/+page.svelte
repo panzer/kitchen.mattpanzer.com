76865d48896f6230142b1570515ada9567a117d3
Matt Panzer <panzer.matthew@gmail.com>
2025-07-21 21:14:34 -0400

    Fix SSG base path

--- src/routes/kitchen/[kitchenId]/+page.svelte
+++ src/routes/kitchen/[kitchenId]/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import { Heading, A } from 'flowbite-svelte';
 	import { page } from '$app/state';
+
+	import { LinkHandler } from '$lib';
 </script>
 
 <Heading>Kitchen {page.params['kitchenId']}</Heading>
-<A href={`/kitchen/${page.params['kitchenId']}/pantry`}>Pantry</A>
-<A href={`/kitchen/${page.params['kitchenId']}/eat`}>Eat</A>
+<A href={LinkHandler(`/kitchen/${page.params['kitchenId']}/pantry`)}>Pantry</A>
+<A href={LinkHandler(`/kitchen/${page.params['kitchenId']}/eat`)}>Eat</A>
 
-<A href={`/kitchen/${page.params['kitchenId']}/eat/add-to-cart`}>Cart</A>
+<A href={LinkHandler(`/kitchen/${page.params['kitchenId']}/eat/add-to-cart`)}>Cart</A>
